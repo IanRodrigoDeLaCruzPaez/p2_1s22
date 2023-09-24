49776ea6f33ef21caee265339f45e3816b365a05
Ian Rodrigo De La Cruz Paez <144197726+IanRodrigoDeLaCruzPaez@users.noreply.github.com>
2023-09-23 21:56:58 -0600

    Add files via upload

---
+++ p2_1s22.java
@@ -0,0 +1,9 @@
+class p2_1s22{
+	public static void main(String[] args) {
+	    System.out.println("Ian Rodrigo De La Cruz Paez");
+	    System.out.println("17 años");
+	    System.out.println("iandrp@hotmail.com");
+	    System.out.println("me gusta jugar futbol");
+	    System.out.println("mi color favorito es el turquesa");
+	}
+}
